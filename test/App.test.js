dd6028b3d7ec8181fd610b9de266124b8563cc63
Sunny Edogbo <sunny.edogbo@andela.com>
2019-01-07 17:27:29 +0100

    [Chore #162688176] Update TravisCI to run Tests [Finishes #162688176]

---
+++ test/App.test.js
@@ -0,0 +1,14 @@
+/* eslint-disable no-undef */
+import React from 'react';
+import { shallow } from 'enzyme';
+import App from '../src/App';
+
+describe('App', () => {
+  test('should match snapshot', () => {
+    const wrapper = shallow(<App />);
+    // console.log(wrapper.debug());
+    expect(wrapper.find('div').text()).toBe('hello world');
+    // eslint-disable-next-line no-unused-expressions
+    expect(wrapper).toMatchSnapshot;
+  });
+});
