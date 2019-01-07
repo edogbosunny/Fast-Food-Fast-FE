f3dbaa77859820a4972e9785eb3b97350b3f9c8d
Sunny Edogbo <sunny.edogbo@andela.com>
2019-01-07 17:35:44 +0100

    [Chore #162688176] Update TravisCI to run Tests [Finishes #162688176]

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 // import './styles/global.scss';
 
+// eslint-disable-next-line react/prefer-stateless-function
 class App extends Component {
   render() {
     return (
       <div>
         hello world
       </div>
-    )
+    );
   }
 }
 
